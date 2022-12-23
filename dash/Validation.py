d07ff5002779e53bd520a83821e45fb3887e30b2
muntashiralarefin <101044582+muntashiralarefin@users.noreply.github.com>
2022-12-23 06:49:56 +0600

    V2_DB-02
    Add Sex
    
    DB Tourist

---
+++ dash/Validation.py
@@ -0,0 +1,7 @@
+# # Validator functions
+# def validate_a2(value):
+#     if value >99 or value < 10:
+#         raise ValidationError(
+#             _('Age should be 10 to 99'),
+#             params={'value': value},
+#         )
--- dash/models.py
+++ dash/models.py
@@ -3,21 +3,17 @@ from django.db import models
 # from django.core.exceptions import ValidationError
 # from django.utils.translation import gettext_lazy as _
 
-# # Validator functions
-# def validate_a2(value):
-#     if value >99 or value < 10:
-#         raise ValidationError(
-#             _('Age should be 10 to 99'),
-#             params={'value': value},
-#         )
-
-
 # # Create your models here.
 
 # ## Main
-# class tourist(models.Model):
-#     a1_name = models.CharField('Tourist name',max_length=30)
-#     a2_age = models.IntegerField('Age',  validators=[validate_a2])
-#     a3_gender = models.CharField('Gender',max_length=30)
-#     def __str__(self):
-#         return self.a1_name
+class Sex(models.Model):
+    sex = models.CharField('Sex',max_length=30)
+    def __str__(self):
+        return self.asex
+
+class Tourist(models.Model):
+    a1_name = models.CharField('Tourist name',max_length=30)
+    a2_age = models.IntegerField('Age')
+    a3_gender = models.CharField('Gender',max_length=30)
+    def __str__(self):
+        return self.a1_name
